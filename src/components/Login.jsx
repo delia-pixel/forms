18f1d8a890beadea3481c9b13fc7f1a3e40d1668
delia-pixel <tpatriciareinedia@gmail.com>
2024-02-02 03:32:17 +0100

    getting user input with state, ref and fromData

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,22 +1,43 @@
+import { useRef } from "react";
+
 export default function Login() {
+  const email = useRef();
+  const password = useRef();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const enteredValues = {
+      email: email.current.value,
+      password: password.current.value,
+    };
+
+    console.log(enteredValues);
+  };
+
+  const handleReset = () => {
+    // email.current.value='' Not recommended to update the DOM manually better use a button type reset
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>Login</h2>
 
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input id="email" type="email" name="email" ref={email} />
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input id="password" type="password" name="password" ref={password} />
         </div>
       </div>
 
       <p className="form-actions">
-        <button className="button button-flat">Reset</button>
+        <button type="reset" className="button button-flat">
+          Reset
+        </button>
         <button className="button">Login</button>
       </p>
     </form>
